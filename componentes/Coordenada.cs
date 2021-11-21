7580d18725f02fce31c24c27547a73e3b0ff013f
renato_sans <renato@datacopy.com.br>
2021-11-21 08:51:34 -0300

    Commit inicial do projeto

---
+++ componentes/Coordenada.cs
@@ -0,0 +1,22 @@
+﻿namespace triangulo.componentes
+{
+    public class Coordenada
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Coordenada(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public String GetX(){
+            return Convert.ToString(this.x) + "px";
+        }
+
+        public String GetY(){
+            return Convert.ToString(this.y) + "px";
+        }
+    }
+}
---
+++ componentes/CorRGB.cs
@@ -0,0 +1,15 @@
+﻿namespace triangulo.componentes
+{
+    public class CorRGB
+    {
+        public Byte R { get; set; }
+        public Byte G { get; set; }
+        public Byte B { get; set; }
+
+        public CorRGB(int R, int G, int B) {
+            this.R = Convert.ToByte(R);
+            this.G = Convert.ToByte(G);
+            this.B = Convert.ToByte(B);
+        }
+    }
+}
